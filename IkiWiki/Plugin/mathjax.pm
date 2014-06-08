b078ec0477e5eea2daefbe0a300fd2c0229445ee
Baldur Kristinsson <baldur.kristinsson@gmail.com>
2014-06-08 21:20:29 +0000

    Removing superfluous _scriptblock (thanks, rbrito)

--- IkiWiki/Plugin/mathjax.pm
+++ IkiWiki/Plugin/mathjax.pm
@@ -68,29 +68,4 @@ sub _scripttag {
       . '"></script>';
 }
 
-sub _scriptblock {
-    return qq%
-<script type="text/javascript">//<![CDATA[
-(function () {
-  if (window.MathJax && MathJax.Hub) return;
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js";
-
-  var config = 'MathJax.Hub.Config({' +
-                 'extensions: ["tex2jax.js"],' +
-                 'jax: ["input/TeX","output/HTML-CSS"],' +
-                 'TeX: { equationNumbers: { autoNumber: "AMS" } }' +
-               '});' +
-               'MathJax.Hub.Startup.onload();';
-
-  if (window.opera) {script.innerHTML = config}
-               else {script.text = config}
-
-  document.getElementsByTagName("head")[0].appendChild(script);
-})();
-//]]></script>
-%;
-}
-
 1;
